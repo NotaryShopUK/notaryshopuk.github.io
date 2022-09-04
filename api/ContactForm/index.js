3ccf7b67dddbf2c84641d63cc627e666c9c93d98
Dermot Winters <jdwinters2001@yahoo.co.uk>
2022-09-04 21:02:55 +0100

    feat: create basic api

---
+++ api/ContactForm/index.js
@@ -0,0 +1,22 @@
+module.exports = async function (context, req) {
+	context.log("JavaScript HTTP trigger function processed a request.");
+
+	const { name, email, query } = req.body;
+
+	context.res = {
+		// status: 200, /* Defaults to 200 */
+		body: "Hello!",
+	};
+
+	context.email = {
+		personalizations: [{
+			to: [{ email: "dermot.winters@notaryshop.co.uk" }],
+		}],
+		from: { email: "api@notaryshop.co.uk" },
+		subject: "Enquiry via contact form",
+		content: [{
+			type: "text/plain",
+			value: [name, email, query].join("\r\n\r\n"),
+		}],
+	};
+}
