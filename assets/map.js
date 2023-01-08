d3821a601f54a540498742042c2f2a11ce6bd9fe
Dermot Winters <jdwinters2001@yahoo.co.uk>
2023-01-08 23:17:35 +0000

    feat: add directions button to map

--- assets/map.js
+++ assets/map.js
@@ -1,3 +1,25 @@
+class DirectionsButton extends ol.control.Control {
+	constructor() {
+		const label = document.createElement("span");
+		label.innerText = "Get directions";
+
+		const button = document.createElement("button");
+		button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 3.515L3.515 12 12 20.485 20.485 12 12 3.515zm.707-2.122l9.9 9.9a1 1 0 0 1 0 1.414l-9.9 9.9a1 1 0 0 1-1.414 0l-9.9-9.9a1 1 0 0 1 0-1.414l9.9-9.9a1 1 0 0 1 1.414 0zM13 10V7.5l3.5 3.5-3.5 3.5V12h-3v3H8v-4a1 1 0 0 1 1-1h4z"/></svg>`;
+		button.appendChild(label);
+
+		const element = document.createElement("div");
+		element.className = "directions ol-unselectable ol-control";
+		element.appendChild(button);
+
+		super({ element });
+
+		button.addEventListener(
+			"click",
+			() => window.open(container.getAttribute("data-directions")),
+		);
+	}
+}
+
 const container = document.getElementById("map");
 
 const lonLat = container
@@ -25,6 +47,7 @@ const vectorSource = new ol.source.Vector({
 
 const map = new ol.Map({
 	target: container,
+	controls: ol.control.defaults.defaults().extend([new DirectionsButton()]),
 	layers: [
 		new ol.layer.Tile({
 			source: new ol.source.OSM(),
